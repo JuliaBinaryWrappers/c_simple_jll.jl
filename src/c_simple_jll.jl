2f645f65cf5cd7e1cd638f8a0f80b381ece1bad8
Elliot Saba <staticfloat@gmail.com>
2019-07-21 19:43:56 -0700

    c_simple_jll build 1.2.3+3

--- src/c_simple_jll.jl
+++ src/c_simple_jll.jl
@@ -9,6 +9,6 @@ platforms = Platform[
 best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
 
 # Load the appropriate wrappers
-include(joinpath(@__DIR__, "wrappers", triplet(best_platform)))
+include(joinpath(@__DIR__, "wrappers", best_platform))
 
 end  # module c_simple_jll
